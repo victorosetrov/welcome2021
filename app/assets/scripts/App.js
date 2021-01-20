9a47e8fe83d7e6f61285255c64b17fb73fb74174
Viktor <victorosetrov@gmail.com>
2021-01-20 18:00:09 +0000

    Completed LightBox Overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,9 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
+new Modal();
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
