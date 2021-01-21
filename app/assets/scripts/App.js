18aee89b8da6c597f16ede14dbc81f5918268e10
Viktor <victorosetrov@gmail.com>
2021-01-21 10:53:52 +0000

    Added lazy-loading for images.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,29 @@
 import '../styles/styles.css'
+import 'lazysizes'
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
 
-new Modal();
-let stickyHeader = new StickyHeader();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
-let mobileMenu = new MobileMenu();
+new MobileMenu();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == 'undefined') {
+            import(/*webpackChunkName: "modal"*/'./modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log('There was a problem.'))
+        } else {
+            modal.openTheModal()
+        }
+
+    })
+})
 
 if (module.hot) {
     module.hot.accept()
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -3,21 +3,17 @@ class Modal {
         this.injectHTML()
         this.modal = document.querySelector(".modal")
         this.closeIcon = document.querySelector(".modal__close")
-        this.openModalButtons = document.querySelectorAll('.open-modal')
         this.events()
     }
 
     events() {
-        //listen for open click
-        this.openModalButtons.forEach(el => el.addEventListener("click", e => this.openTheModal(e)))
         //listen for close click
         this.closeIcon.addEventListener("click", () => this.closeTheModal())
         //pushes any key
         document.addEventListener("keyup", e => this.keyPressHandler(e))
     }
 
-    openTheModal(e) {
-        e.preventDefault()
+    openTheModal() {
         this.modal.classList.add("modal--is-visible")
     }
 
