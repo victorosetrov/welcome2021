55f16b7da8023f17bdc2e2ab0c2c7ac35d942993
Viktor <victorosetrov@gmail.com>
2021-01-25 14:28:31 +0000

    Have updated secret-area.js

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,22 +1,42 @@
 exports.handler = function(event, context, callback) {
+    const headers = {
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Headers" : "Content-Type"
+    }
+   
+    if (event.httpMethod !== "POST") {
+      return callback(null, {
+        statusCode: 200,
+        headers,
+        body: "This was not a POST request"
+      })
+    }
+    
     const secretContent = `
-    <h3> Welcome to the secret area </h3>
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
     `
-    let body 
+    
+    let body
+   
     if (event.body) {
-        body = JSON.parse(event.body)
-    } else { 
-        body = {}
-    }
-
-    if (body.parser == 'javascript') {
-        callback(null, {
-            statusCode: 200,
-            body: secretContent
-        })
+      body = JSON.parse(event.body)
     } else {
-        callback(null, {
-            statusCode: 401,
-        })
+      body = {}
     }
-}
+   
+    if (body.password == "javascript") {
+      callback(null, {
+        statusCode: 200,
+        headers,
+        body: secretContent
+      })
+    } else {
+      callback(null, {
+        statusCode: 401,
+        headers
+      })
+    }
+   
+    
+  }
