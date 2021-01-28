a85d76900c7960a079ac47f03d6827f6d12a3db5
Viktor <victorosetrov@gmail.com>
2021-01-28 16:34:07 +0000

    Completed my personal web-site

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -34,14 +34,13 @@ class Modal {
     <div class="modal__inner">
       <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
       <div class="wrapper wrapper--narrow">
-        <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
+        <p class="modal__description">Thanks for taking the time to reach out. How can I help you today?</p>
       </div>
 
       <div class="social-icons">
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/images/icons/youtube.svg" alt="YouTube"></a>
+        <a href="https://www.facebook.com/victor.osetrov/" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
+        <a href="https://twitter.com/ViktorOsetrov" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
+        <a href="https://www.instagram.com/viktorosetrov/" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
       </div>
     </div>
     <div class="modal__close">X</div>
